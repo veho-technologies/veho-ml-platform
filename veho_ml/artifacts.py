064ab371e1d9fc1816e3a9f2d69d7d1a6ac48169
MattVeho <matthew.dinardo@shipveho.com>
2024-01-01 18:15:37 -0800

    Fix import

--- veho_ml/artifacts.py
+++ veho_ml/artifacts.py
@@ -1,14 +1,10 @@
 import abc
 import mlflow
 
-from config import config
+from veho_ml.config import config
 
 
 class VehoArtifactRegistry(abc.ABC):
-    def __init__(self):
-        mlflow.set_tracking_uri(config.mlflow_tracking_uri)
-        self.mlflow_client = mlflow.MlflowClient()
-        
     @abc.abstractmethod
     def register_artifacts(self):
         """Method that must be implemented in a subclass"""
